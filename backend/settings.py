e6eaf80b393175916e521c4c4045337f95241558
eduard-if <eduard-dev@proton.me>
2023-06-04 12:36:36 +0300

    Model Color Picking
    https://github.com/fabiocaccamo/django-colorfield

--- backend/settings.py
+++ backend/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'rest_framework',
 
     'corsheaders',
+
+    'colorfield',
 ]
 
 
@@ -125,7 +127,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/images/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static/base/',
+]
+
+MEDIA_ROOT = 'static/base/images'
+
+STATIC_ROOT = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- base/models.py
+++ base/models.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 
 import uuid
 
+from colorfield.fields import ColorField
+
 
 class Timeline(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
@@ -12,6 +14,10 @@ class Timeline(models.Model):
     createdAt = models.DateTimeField(auto_now_add=True)
     lastUpdated = models.DateTimeField(auto_now=True)
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    bgColor = ColorField(format='hex')
+    textColor = ColorField(format='hex')
+    titleColor = ColorField(format='hex')
+    borderColor = ColorField(format='hex')
 
     def __str__(self):
         return self.title
