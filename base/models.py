fc1d359d7d6a2c45f8658b2379fb06d874c53d1b
eduard-if <eduard-dev@proton.me>
2023-07-12 11:26:21 +0300

    Extra event item fields

--- base/models.py
+++ base/models.py
@@ -25,12 +25,22 @@ class Timeline(models.Model):
 
 class Item(models.Model):
     timeline = models.ForeignKey(Timeline, on_delete=models.CASCADE)
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(max_length=255, null=False, blank=True)
+    # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=255, null=True, blank=True)
     start = models.DateTimeField(null=False, blank=False)
     end = models.DateTimeField(null=True, blank=True)
     type = models.CharField(max_length=10, null=True, blank=True)
-    content = models.TextField(null=True, blank=False)
+    content = models.TextField(null=False, blank=False)
+    selectable = models.BooleanField(default=True)
+    style = models.CharField(max_length=255,  null=True, blank=True)
+    notesDetails = models.TextField(null=True, blank=True)
+
+    bgColor = models.CharField(max_length=255, null=True, blank=True)
+    textColor = models.CharField(max_length=255, null=True, blank=True)
+    borderColor = models.CharField(max_length=255, null=True, blank=True)
+    fontSize = models.CharField(max_length=255, null=True, blank=True)
+    fontStyle = models.CharField(max_length=255, null=True, blank=True)
+    fontWeight = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
-        return self.title
+        return self.content
